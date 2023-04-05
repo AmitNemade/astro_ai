84bdce17d48b8f8d2b3f958b14b517d22861bdde
Amit Nemade <amitnemade34@gmail.com>
2023-04-05 16:39:36 +0530

    Updated error message

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,8 +1,12 @@
 import jwt from "jsonwebtoken";
+import _ from "lodash";
 
 export const authMiddleware = async (req, res, next) => {
   try {
     const token = req.headers["authorization"];
+    if (_.isEmpty(token))
+      return res.status(442).send({ success: false, error_message: "Please login to use AstroAI." });
+
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         return res
